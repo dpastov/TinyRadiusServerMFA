32476a5cfbdb02fd52505b1eae0ec5e038c9c2ba
dpastov <dpastov@gmail.com>
2024-05-22 20:08:58 +0200

    first version of tinyradius server with twilio and challange

--- src/main/java/TinyRadiusServerMFA.java
+++ src/main/java/TinyRadiusServerMFA.java
@@ -15,26 +15,36 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
-public class MyTinyRadiusServer extends RadiusServer {
+public class TinyRadiusServerMFA extends RadiusServer {
 	private Map<String, String> challenges = new HashMap<>();
-	private Map<String, String> userPhoneNumbers = new HashMap<>();
-
-	public MyTinyRadiusServer() {
+	private HashMap<String, HashMap<String,String>> users = new HashMap<String, HashMap<String, String>>();
+	private String m_secret = "";
+	
+	public TinyRadiusServerMFA() {
 		try {
+			System.out.println("Loading data from config.properties...");
 			Properties properties = new Properties();
 			FileInputStream fis = new FileInputStream("config.properties");
 			properties.load(fis);
 			fis.close();
 
+			m_secret = properties.getProperty("radius.secret");
+			
 			// Read properties
 			String twilio_sid = properties.getProperty("twilio.sid");
 			String twilio_token = properties.getProperty("twilio.token");
+
 			String user_name = properties.getProperty("user.name");
 			String user_phone = properties.getProperty("user.phone");
-
+			String user_password = properties.getProperty("user.password");
+			HashMap<String, String> user = new HashMap<String, String>();
+			user.put("name", user_name);
+			user.put("phone", user_phone);
+			user.put("password", user_password);
+			users.put(user_name, user);
+			
 			// Initialize Twilio
 			Twilio.init(twilio_sid, twilio_token);
-			userPhoneNumbers.put(user_name, user_phone);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -42,14 +52,15 @@ public class MyTinyRadiusServer extends RadiusServer {
 
 	@Override
 	public String getSharedSecret(InetSocketAddress client) {
-		return "sharedSecret123";
+		return m_secret;
 	}
 
 	@Override
-	public String getUserPassword(String userName) {
-		if (userName.equals("John")) {
-			return "token123";
+	public String getUserPassword(String username) {
+		if (users.containsKey(username)) {
+			users.get(username).get("password");
 		}
+		
 		return null;
 	}
 
@@ -68,7 +79,7 @@ public class MyTinyRadiusServer extends RadiusServer {
 		}
 
 		if (password.equals(getUserPassword(username))) {
-			String userPhoneNumber = userPhoneNumbers.get(username);
+			String userPhoneNumber = users.get(username).get("password");
 			if (userPhoneNumber != null) {
 				String code = generateVerificationCode();
 				challenges.put(username, code);
@@ -80,7 +91,6 @@ public class MyTinyRadiusServer extends RadiusServer {
 		return new RadiusPacket(RadiusPacket.ACCESS_REJECT, request.getPacketIdentifier());
 	}
 
-	@Override
 	public RadiusPacket accessRequestReceived(AccessRequest request, InetSocketAddress client) {
 		return accessRequestReceived(request);
 	}
@@ -98,11 +108,11 @@ public class MyTinyRadiusServer extends RadiusServer {
 	private void sendSms(String to, String code) {
 		Message message = Message.creator(new PhoneNumber(to), new PhoneNumber("+12172161812"),
 				"Your verification code is: " + code).create();
-		System.out.println("Sent message to " + to + ": " + message.getSid());
+		System.out.println("Sent message to " + to + ": " + message.getBody());
 	}
 
 	public static void main(String[] args) {
-		MyTinyRadiusServer server = new MyTinyRadiusServer();
+		TinyRadiusServerMFA server = new TinyRadiusServerMFA();
 		try {
 			server.start(true, true);
 		} catch (Exception e) {
